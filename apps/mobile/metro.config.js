6813427c60952656c777f4ae7a7523cfca18a32c
Devkdy22 <devkdy99@gmail.com>
2026-07-27 12:19:31 +0900

    [front] 모바일 알림과 Timey 런타임 설정 추가

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -8,6 +8,11 @@ const workspaceRoot = path.resolve(projectRoot, '../..');
 
 const config = getDefaultConfig(projectRoot);
 
+// Rive files are binary Metro assets and must be bundled through require().
+if (!config.resolver.assetExts.includes('riv')) {
+  config.resolver.assetExts.push('riv');
+}
+
 config.watchFolders = [workspaceRoot];
 config.resolver.nodeModulesPaths = [
   path.resolve(projectRoot, 'node_modules'),
